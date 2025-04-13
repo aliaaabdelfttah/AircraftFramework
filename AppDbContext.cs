4570c62511dda205a2d9bb1f4a2749d51244f443
Aliaa Abdelfttah <abdelfttahaliaa@gmail.com>
2025-04-13 13:02:00 +0200

    Update AppDbContext.cs

--- AppDbContext.cs
+++ AppDbContext.cs
@@ -15,3 +15,34 @@ namespace AirportManagementSystem.Data
         }
     }
 }
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System.IO;
+
+private void ExportToPdfButton_Click(object sender, EventArgs e)
+{
+    using (var db = new AppDbContext())
+    {
+        var aircrafts = db.Aircrafts.ToList();
+        var document = new Document();
+        var filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "Aircrafts.pdf");
+
+        PdfWriter.GetInstance(document, new FileStream(filePath, FileMode.Create));
+        document.Open();
+
+        document.Add(new Paragraph("Aircrafts Data"));
+        document.Add(new Paragraph("\n"));
+
+        foreach (var aircraft in aircrafts)
+        {
+            document.Add(new Paragraph($"Aircraft Number: {aircraft.AircraftNumber}"));
+            document.Add(new Paragraph($"Model: {aircraft.Model}"));
+            document.Add(new Paragraph($"Capacity: {aircraft.Capacity}"));
+            document.Add(new Paragraph($"Manufacturer: {aircraft.Manufacturer}"));
+            document.Add(new Paragraph("\n"));
+        }
+
+        document.Close();
+        MessageBox.Show($"Data exported to {filePath}");
+    }
+}
